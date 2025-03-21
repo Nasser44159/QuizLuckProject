37c752a002ef2b5c2d9f94a3d8514bd242273a3c
Nasser <nasser.sentissi2@gmail.com>
2025-03-21 16:23:44 +0000

    Sécurisation des credentials email

--- QuizLuck/settings.py
+++ QuizLuck/settings.py
@@ -169,6 +169,8 @@ STATICFILES_DIRS = [
 
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -179,5 +181,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Par exemple, pour Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nasser.sentissi2@gmail.com'
-EMAIL_HOST_PASSWORD = 'zegf ngyv bign pbkq'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
